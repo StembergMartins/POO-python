9d0cddf56c215b8902d2341e96fafbaa7c16b6b6
STEMBERG MARTINS DA SILVA CARVALHO <stemberg.martins@gmail.com>
2023-01-12 23:05:31 -0300

    aluna 03 POO

--- formulario.py
+++ formulario.py
@@ -1,6 +1,6 @@
 class Caneta:
 
-    def __init__(self, modelo, cor, ponta = float, carga = int, tampa = bool):
+    def __init__(self, modelo = str, cor = str, ponta = float, carga = int, tampa = bool):
         self.modelo = modelo
         self.cor = cor
         self._ponta = ponta
@@ -8,10 +8,7 @@ class Caneta:
         self.__tampa = tampa
 
     def escrever(self, frase):
-        if self.tampa:
-            print(f"Para escrever com a caneta {self.modelo} deverá está sem tampa! ")
-            return
-        print(f"A caneta de modelo {self.modelo} escreveu em {self.cor} a frase: {frase}")
+        None
 
     def rabiscar(self):
         None
--- main.py
+++ main.py
@@ -7,7 +7,6 @@ p1 = Pessoa("stemberg", 28)
 c1 = Caneta("Bic", "Azul", 0.5, 80, 1)
 
 c1.escrever("olá mundo em POO!")
-c1.remover_tampa()
-c1.remover_tampa()
-c1.escrever("olá mundo em POO!")
 
+
+print(vars(c1))
